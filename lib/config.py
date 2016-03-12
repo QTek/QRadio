5f8a236e79f3ce93f16bdde4331bf1d90b6affac
10TOHH <max.leshchyna@gmail.com>
2016-03-11 19:46:49 -0500

    Add registration links

--- lib/config.py
+++ lib/config.py
@@ -1,23 +1,33 @@
+# This file contains API keys that are required in order to pull data from sources
+# You can register for those platforms by provided links.
+
 # Cymon
+# Registration: https://cymon.io/user/signup
 cymon_api_key = ''
 
 # VirusTotal
+# Registration: https://www.virustotal.com/
 virustotal_api_key = ''
 
 # Malwr
+# Registration: https://malwr.com/account/signup/
 malwr_login = ''
 malwr_passwd = ''
 malw_api_key = ''
 
 # Totalhash
+# Registration: https://totalhash.cymru.com/contact-us/
 totalhash_uid = ''
 totalhash_api_key = ''
 
 # MetaScan
+# Registration: https://accounts.opswat.com/auth/module.php/opswauth/register.php
 metascan_api_key = ''
 
 # PassiveTotal
+# Registration: https://www.passivetotal.org/register
 passivetotal_api_key = ''
 
 # IBM X-Force Exchange
+# Registration: https://www.ibm.com/account/profile/us?page=reg
 ibmxforce_token = None
