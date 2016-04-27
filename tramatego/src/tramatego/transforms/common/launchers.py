109461e0c87e21278db78b9d94b4b678230920e2
Zappus <zappus@protonmail.com>
2016-04-27 10:44:08 -0400

    fixed launchers.py
    Updated the function to avoid junk data from appearing.

--- tramatego/src/tramatego/transforms/common/launchers.py
+++ tramatego/src/tramatego/transforms/common/launchers.py
@@ -2,19 +2,25 @@ import subprocess
 
 
 def get_qradio_data(command, result_column = 0 ):
-    python_path = 'python'
-    qradio_path = 'path_to_cli_qradio.py'
+    python_path = 'C:\Python27\python.exe'
+    qradio_path = 'C:\MaltegoTransforms\QRadio\cli_qradio.py'
+    error = "lol"
+    output = []
 
-    command_final = python_path + " " + qradio_path + ' ' + command
+    command_final = python_path + ' ' + qradio_path + ' ' + command
     p = subprocess.Popen(command_final, stdout=subprocess.PIPE, shell=True)
     out, err = p.communicate()
-    result = out.split('\n')
-    output = []
+    try:
+        result = out.split('\n')
+        del result[0]
+    except:
+        error = "yay..."
+
     for lin in result:
-        if not lin.startswith('#'):
-			try:
-				final_out = lin.split(',')[result_column]
-				output.append(final_out)
-			except:
-				output.append("Error in Transform")
+        try:
+            final_out = lin.split(',')[result_column].strip("\n").strip("\r")
+            if final_out != "" and final_out != " ":
+                output.append(final_out)
+        except:
+            error = 'oh no...'
     return output
